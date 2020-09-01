b15458ee085de985e34c8369b32ace925fe89d23
Publio Quintero <publio.quintero@gmail.com>
2020-09-01 00:01:41 -0600

    res

--- resources/js/components/functions.js
+++ resources/js/components/functions.js
@@ -1,3 +1,3 @@
 // export const URLserver = 'http://localhost:8000/';
 
-export const URLserver = 'https://brenwmotion.com/minChat/public/';
+export const URLserver = 'https://chat.brewnmotion.com';
