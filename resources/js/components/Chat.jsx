321e4b087e8fa5b1405c9c602ec0307d8cf171f0
Publio Quintero <publio.quintero@gmail.com>
2020-09-01 00:12:28 -0600

    changes

--- resources/js/components/Chat.jsx
+++ resources/js/components/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
     return (
         <div className="chat">
             {Object.entries(main.messages).map( ([key, value]) => 
-                <div className="animate__animated animate__fadeInUp animate__faster text-blue-600 bg-blue-200 m-2 p-2 text-2xl" key={key}>{value.message}</div>
+                <div className="animate__animated animate__fadeInUp animate__faster text-blue-600 bg-blue-200 m-2 p-2 text-4xl" key={key}>{value.message}</div>
             )}
         </div>
     )
--- resources/js/components/TextArea.jsx
+++ resources/js/components/TextArea.jsx
@@ -23,16 +23,16 @@ const TextArea = () => {
     };
     return (
         <div className="w-full text">
-            <form action="" onSubmit={handleSubmit} className="flex justify-content w-full">
+            <form action="" onSubmit={handleSubmit} className="w-full">
                 <textarea
                     name="message"
                     id="message"
                     rows="3"
-                    className="appearance-none block w-10/12 bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-2xl"
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 text-4xl"
                     onChange={handleChange}
                     value={data.message}
                 ></textarea>
-                <button className="w-2/12 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                <button className="w-full text-2xl bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
                     Enviar
                 </button>
             </form>
