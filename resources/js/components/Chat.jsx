d3d8895670f138e88d928d172b1fad94be43602a
Publio Quintero <publio.quintero@gmail.com>
2020-09-01 07:54:11 -0600

    Update Chat.jsx

--- resources/js/components/Chat.jsx
+++ resources/js/components/Chat.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 const Chat = () => {
     const main = useSelector( state => state.mainReducers)
     return (
-        <div className="bg-gray-100 w-full h-64 overflow-y-scroll">
+        <div className="bg-gray-100 w-8/12 m-auto h-64 overflow-y-scroll">
             {Object.entries(main.messages).map( ([key, value]) => 
                 <div className="text-xs animate__animated animate__fadeInUp animate__faster text-blue-600 bg-blue-200 m-2 p-2" key={key}>{value.message}</div>
             )}
