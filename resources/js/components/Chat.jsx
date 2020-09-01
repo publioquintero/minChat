63845f1b52daf58635de8bcd2daf4800db054f0b
Publio Quintero <publio.quintero@gmail.com>
2020-09-01 09:55:26 -0600

    Update Chat.jsx

--- resources/js/components/Chat.jsx
+++ resources/js/components/Chat.jsx
@@ -6,7 +6,7 @@ const Chat = () => {
     return (
         <div className="bg-gray-100 w-8/12 m-auto h-64 overflow-y-scroll">
             {Object.entries(main.messages).map( ([key, value]) => 
-                <div className="text-xs animate__animated animate__fadeInUp animate__faster text-blue-600 bg-blue-200 m-2 p-2" key={key}>{value.message}</div>
+                <div className="text-xs animate__animated animate__fadeInDown animate__faster text-blue-600 bg-blue-200 m-2 p-2" key={key}>{value.message}</div>
             )}
         </div>
     )
