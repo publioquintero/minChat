b258d32170f3e55d1a70cbeed1eded8dc47640a2
Publio Quintero <publio.quintero@gmail.com>
2020-09-05 15:52:20 -0600

    trs

--- resources/js/components/Logo.jsx
+++ resources/js/components/Logo.jsx
@@ -3,7 +3,7 @@ import LogoImg from '../components/assets/images/logo.png'
 
 const Logo = () => {
     return (
-        <div className="bg-red-200 w-full h-20" style={{ backgroundImage: 'url('+LogoImg+')', backgroundSize:'cover', backgroundPosition: 'center center' }}></div>
+        <div className="w-full h-20" style={{ backgroundImage: 'url('+LogoImg+')', backgroundSize:'contain', backgroundPosition: 'center center' }}></div>
     )
 }
 
